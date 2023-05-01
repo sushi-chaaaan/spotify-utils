9c8e49de69a02f843268eaffb28054acb3b51209
sushi-chaaaan <mail@sushichan.live>
2023-05-01 16:06:04 +0900

    fix

--- functions/api/track.ts
+++ functions/api/track.ts
@@ -1,15 +1,14 @@
 export const onRequestGet: PagesFunction<Bindings> = async (ctx) => {
-  const trackUrl = new URL(ctx.request.clone().url).searchParams.get('url')
+  const trackUrl = new URL(ctx.request.url).searchParams.get('url')
 
   if (!trackUrl) {
     return new Response('Missing url parameter', { status: 400 })
   }
 
-  const baseUrl = 'https://now-playing.sushichan.live'
-
-  const req = new Request(`${baseUrl}?url=${trackUrl}`, ctx.request.clone())
-
-  return new Response(JSON.stringify(req.body))
+  const req = new Request(
+    'https://now-playing.sushichan.live',
+    ctx.request.clone()
+  )
 
   return ctx.env.API_WORKER.fetch(req)
 }
