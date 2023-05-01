5cd396d27201218b83e08695843e5fe1ea51ca55
sushi-chaaaan <mail@sushichan.live>
2023-05-01 15:56:24 +0900

    clone request

--- functions/api/track.ts
+++ functions/api/track.ts
@@ -1,5 +1,5 @@
 export const onRequestGet: PagesFunction<Bindings> = async (ctx) => {
-  const trackUrl = new URL(ctx.request.url).searchParams.get('url')
+  const trackUrl = new URL(ctx.request.clone().url).searchParams.get('url')
 
   if (!trackUrl) {
     return new Response('Missing url parameter', { status: 400 })
