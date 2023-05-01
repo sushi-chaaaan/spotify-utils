13c7f51a3d4a5f15bcc8c5f13d556bd9be5f66d1
sushi-chaaaan <mail@sushichan.live>
2023-05-01 16:17:30 +0900

    fix origin

--- functions/api/track.ts
+++ functions/api/track.ts
@@ -4,7 +4,7 @@ export const onRequestGet: PagesFunction<Bindings> = async (ctx) => {
   if (!trackUrl) {
     return new Response('Missing url parameter', { status: 400 })
   }
-  const req = new Request(new URL(ctx.request.url).host, ctx.request)
+  const req = new Request(new URL(ctx.request.url).origin, ctx.request)
 
   return ctx.env.API_WORKER.fetch(req)
 }
