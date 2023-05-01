bf2da6a91dd66a1414a845d11fb361ecaab25c1f
sushi-chaaaan <mail@sushichan.live>
2023-05-01 16:24:28 +0900

    set query

--- functions/api/track.ts
+++ functions/api/track.ts
@@ -1,10 +1,12 @@
 export const onRequestGet: PagesFunction<Bindings> = async (ctx) => {
-  const trackUrl = new URL(ctx.request.url).searchParams.get('url')
+  const requestUrl = new URL(ctx.request.clone().url)
+
+  const trackUrl = requestUrl.searchParams.get('url')
 
   if (!trackUrl) {
     return new Response('Missing url parameter', { status: 400 })
   }
-  const req = new Request(new URL(ctx.request.url).origin, ctx.request)
+  const req = new Request(`${requestUrl.origin}?url=${trackUrl}`, ctx.request)
 
   return ctx.env.API_WORKER.fetch(req)
 }
