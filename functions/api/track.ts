236d1a21c0c969b7eff788e7d7b2496b31849e84
sushi-chaaaan <mail@sushichan.live>
2023-05-01 15:48:45 +0900

    fix req obj

--- functions/api/track.ts
+++ functions/api/track.ts
@@ -7,10 +7,7 @@ export const onRequestGet: PagesFunction<Bindings> = async (ctx) => {
 
   const baseUrl = 'https://now-playing.sushichan.live'
 
-  const req = new Request(
-    `${baseUrl}?url=${trackUrl}`,
-    new Request(ctx.request)
-  )
+  const req = new Request(`${baseUrl}?url=${trackUrl}`, ctx.request)
 
   return ctx.env.API_WORKER.fetch(req)
 }
